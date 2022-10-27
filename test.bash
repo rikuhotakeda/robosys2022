6ed00027c7139640a3ed970e823d956acfe85494
Rikuho Takeda <s21c1074ho@s.chibakoudai.jp>
2022-10-27 13:02:45 +0900

    change test.bash

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/bin/bash
 # SPDX-FileCopyrightText: 2022 Takeda Rikuho
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -9,8 +9,18 @@ ng () {
 
 res=0
 
+### I/O ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out= $(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
